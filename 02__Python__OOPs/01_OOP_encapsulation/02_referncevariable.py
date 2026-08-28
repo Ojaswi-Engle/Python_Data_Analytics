160c617f6ac54fa41e93c4ebeabc5e7cd9934a49
ojaswi <ojaswiengle@gmail.com>
2026-08-28 17:40:43 +0530

    Added reference variables concept

--- 02__Python__OOPs/01_OOP_encapsulation/02_referncevariable.py
+++ 02__Python__OOPs/01_OOP_encapsulation/02_referncevariable.py
@@ -11,7 +11,49 @@ class Person:
       print('Hello',self.name)
 
 p=Person('o','i')
-p.gender='male'
+p.gender='male'#if we are trying to access a variable which is not in class then it can be created for that particular object
 print(p.gender)
-#write a function to check prime
 
+#reference variables= variables which hold the reference of objects,basically it does not store the actual data of obj but instead stores its address 
+#pass by refernce 
+class Person:
+  def __init__(self,name):
+    self.name=name
+def greet(person):
+  print(id(person))
+  person.name='mohit'
+
+p=Person('ojaswi')
+print(id(p))
+print(p.name)
+greet(p)  #pass by reference 
+print(p.name)
+
+#return reference of object
+class Person:
+  def __init__(self,name):
+    self.name=name
+def greet(person):
+  p1=Person('Mohit')
+  print(id(p1))
+  return p1
+
+p=Person('Ojaswi')
+p2=greet(p)
+print(p2.name)
+print(id(p2))
+
+#object is mutable
+class Student:
+    def __init__(self):
+      self.name='Ojaswi'
+
+def change(student):
+    student.name='Mohit'
+    return student
+
+s=Student()
+print(id(s))
+s1=change(s)
+print(id(s1))
+print()
