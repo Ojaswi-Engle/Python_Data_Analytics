4577ecb06986aad5c67c66f26d960eefc3864691
ojaswi <ojaswiengle@gmail.com>
2026-08-29 16:14:37 +0530

    Q3 added of task 8

--- 02__Python__OOPs/01_OOP_encapsulation/04_task8.py
+++ 02__Python__OOPs/01_OOP_encapsulation/04_task8.py
@@ -99,3 +99,29 @@ print(d)
 for card in d.cards:
     print(card)
 
+'''Q-3: Find the area of a rectangle.
+Approach:
+
+The class name should be Rectangle.
+The constructor should accept two parameters length and height but you can't pass the values directly to it while creating the constructor. E.g., rectangle = Rectangle(length=10, height=8) <-- you can't do that while creating the instances.
+Create a method called area() which has no parameters.
+Create a method called is_square() which also has no parameters. Return True if the rectangle is a square otherwise return False.
+If you are using a if-else block inside the is_square() method, then use the one-linear syntax.
+'''
+class Rectangle:
+    def __init__(self,length=None,height=None):
+
+        self.length=length
+        self.height=height
+
+    def area(self):
+        return self.length*self.height
+
+    def is_square(self):
+        return self.length==self.height
+r=Rectangle()
+r.length=12
+r.height=4
+print(r.area())
+print(r.is_square())
+    
