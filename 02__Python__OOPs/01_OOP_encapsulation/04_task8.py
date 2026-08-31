e7e89cba6fc30bdb838ce96128008392cf8d99f7
ojaswi <ojaswiengle@gmail.com>
2026-08-31 20:21:18 +0530

    Question 5 and 6 of task 8 added

--- 02__Python__OOPs/01_OOP_encapsulation/04_task8.py
+++ 02__Python__OOPs/01_OOP_encapsulation/04_task8.py
@@ -206,3 +206,227 @@ d9.display_difference()
 
 d10 = Date("29-02-2024")
 d10.display_difference()
+
+'''Q-5: Problem 5
+Statement: A university wants to automate their admission process. 
+Students are admitted based on the marks scored in the qualifying exam.
+ A student is identified by student id, age and marks in qualifying exam. 
+ Data are valid, if:
+
+Age is greater than 20
+Marks is between 0 and 100 (both inclusive)
+A student qualifies for admission, if
+
+Age and marks are valid and
+Marks is 65 or more
+Write a python program to represent the students seeking admission in the university. The details of student class are given below.
+'''
+
+class Student:
+    def __init__(self):
+        self.__student_id=None
+        self.__marks=None
+        self.__age=None 
+
+    def set_student_id(self,student_id):
+        self.__student_id=student_id
+
+    def set_marks(self,marks):
+        self.__marks=marks
+
+    def set_age(self,age):
+        self.__age=age
+
+    def get_student_id(self):
+        return  self.__student_id
+    
+    def get_marks(self):
+        return self.__marks
+    
+    def get_age(self):
+        return  self.__age
+
+    def validate_marks(self):
+        if self.__marks >=0 and self.__marks<=100:
+            return True
+        else:
+            return False
+
+    def validate_age(self):
+        if self.__age>20:
+            return True
+        else:
+            return False
+
+    def check_qualification(self):
+        if self.validate_marks() and self.validate_age():
+            if self.__marks>=65:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+s=Student()
+s.set_student_id(101)
+s.set_marks(65)
+s.set_age(21)
+print(s.check_qualification())
+
+
+s.set_student_id(102)
+s.set_marks(80)
+s.set_age(25)
+print(s.check_qualification())
+
+s.set_student_id(103)
+s.set_marks(90)
+s.set_age(20)
+print(s.check_qualification())
+
+s.set_student_id(101)
+s.set_marks(64)
+s.set_age(21)
+print(s.check_qualification())
+
+s.set_student_id(105)
+s.set_marks(100)
+s.set_age(21)
+print(s.check_qualification())
+
+s.set_student_id(106)
+s.set_marks(0)
+s.set_age(21)
+print(s.check_qualification())
+
+'''Q-6: Ice-Cream Scoops and Bowl shop
+Create a class Scoop which has one public property flavor and one private proptery price. Take flavor values during object creation.
+
+Create a class Bowl with private prperty scoop_list which will have list of scoopd object.
+
+Create a method add_scoops in Bowl class which will add any no of Scoop objects given as parameter and store it in scoops_list.
+
+Make getter and setter method for price property.
+
+Make a method display to display flavour and price of each Scoop in scoop_list and print total price of the bowl by adding all flavour scoops prices.
+
+Make a method sold in both Scoop class and Bowl class to print no of quantity sold.
+
+Ex.-
+
+choco = Scoop('chocolate')
+print(choco)
+choco.set_price(100)
+
+berry = Scoop('berry')
+berry.set_price(120)
+print(berry)
+
+vanilla = Scoop('vanilla')
+vanilla.set_price(150)
+
+bowl = Bowl()
+
+bowl.add_scoops(choco) # Giving one parameter
+bowl.add_scoops(berry, vanilla) # Multiple
+# add_scoops should handle both scenerios
+
+print(bowl)
+
+bowl.display()
+
+Scoop.sold()
+Bowl.sold()
+
+Output
+
+Flavor - chocolate Price - None
+Flavor - berry Price - 120
+chocolate
+berry
+vanilla
+Dsiplaying Bowl
+Flavor - chocolate Price - 100
+Flavor - berry Price - 120
+Flavor - vanilla Price - 150
+Price of Bowl - 370
+3 scoops sold
+1 bowls sold
+'''
+class Scoop:
+    __scoop_sold=0
+
+    def __init__(self,flavour):
+        self.flavour=flavour
+        self.__price=None
+        Scoop.__scoop_sold+=1
+
+    def set_price(self,price):
+        self.__price=price
+
+    def get_price(self):
+        return self.__price
+
+    def __str__(self):
+        return 'Flavour - {} Price - {}'.format(self.flavour,self.__price)
+
+    @staticmethod
+    def sold():
+        print( '{} scoops sold'.format(Scoop.__scoop_sold))
+
+class Bowl:
+    __bowl_sold=0
+
+    def __init__(self):
+        self.__scoop_list=[]
+        Bowl.__bowl_sold+=1
+
+    def add_scoops(self,*args):
+        for scoop in args:
+            self.__scoop_list.append(scoop)
+            print(scoop.flavour)
+
+    def display(self):
+        total_price=0
+        print('Displaying Bowl')
+        for scoop in self.__scoop_list:
+            print(scoop)
+            scoop_price=scoop.get_price()
+            total_price+=scoop_price
+
+        print('Price of Bowl - {}'.format(total_price))
+
+    @staticmethod
+    def sold():
+        print( '{} bowls sold'.format(Bowl.__bowl_sold))
+
+choco = Scoop('chocolate')
+print(choco)
+choco.set_price(100)
+
+berry = Scoop('berry')
+berry.set_price(120)
+print(berry)
+
+vanilla = Scoop('vanilla')
+vanilla.set_price(150)
+
+bowl = Bowl()
+
+bowl.add_scoops(choco) 
+bowl.add_scoops(berry, vanilla) 
+
+bowl.display()
+
+Scoop.sold()
+Bowl.sold()
+
+
+
+
+        
+
+    
+
+
+
